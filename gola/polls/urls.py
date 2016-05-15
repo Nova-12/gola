f5a744ba960c2d76fea63caad525e68131cec904
blukat29 <yunjong@kaist.ac.kr>
2016-05-16 00:15:22 +0900

    Add forms and templates

--- gola/polls/urls.py
+++ gola/polls/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^new/$', views.new, name='new'),
-    url(r'^get/(?P<poll_id>[a-fA-F0-9]+)/$', views.get, name='get'),
+    url(r'^$', views.index, name='polls.index'),
+    url(r'^new/$', views.new, name='polls.new'),
+    url(r'^get/(?P<poll_id>[a-fA-F0-9]+)/$', views.get, name='polls.get'),
 ]
--- gola/polls/views.py
+++ gola/polls/views.py
@@ -1,14 +1,24 @@
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
 
+from .forms import NewPollForm
 from .models import Poll
 
 def index(request):
-    return HttpResponse("Hi!")
+    return render(request, 'polls/index.html', {})
 
 def new(request):
-    poll = Poll.new("aaaa", "bbbb@cccc.com")
-    return HttpResponse(str(poll))
+    if request.method == 'POST':
+        form = NewPollForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            noti_email = form.cleaned_data['noti_email']
+            poll = Poll.new(title, noti_email)
+            return HttpResponseRedirect(reverse('polls.get', args=[poll.poll_id]))
+    else:
+        form = NewPollForm()
+    return render(request, 'polls/new.html', {'form':form})
 
 def get(request, poll_id):
     poll = Poll.get(poll_id)
